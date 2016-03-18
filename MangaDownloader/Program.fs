4dfaf4fdf7b037c8ae08eb1ddcee61009fe93060
David Raab <davidraab83@gmail.com>
2016-03-18 17:15:31 +0100

    Shortened Command-Line commands

--- MangaDownloader/Program.fs
+++ MangaDownloader/Program.fs
@@ -23,15 +23,16 @@ let main argv =
     let (|Uri|_|) input = try Some(new Uri(input)) with exn -> None
     let (|Int|_|) = Int32.tryParse
 
+    let fromTo uri start stop =
+        Manga.fromUri uri >>= Manga.downloadFromTo start stop |> ignore
+
     // Subtracting "-1" from every "no" because in "showChapters" all chapters are shown "1" based 
     // instead of zero based.
     match argv with
-    | [|Uri uri|]         -> showChapters uri
-    | [|Uri uri; "all"|]  -> Manga.fromUri uri >>= Manga.downloadFromTo 0 Int32.MaxValue |> ignore
-    | [|Uri uri; Int no|] -> Manga.fromUri uri >>= Manga.downloadFromTo (no-1) (no-1)    |> ignore
-    | [|Uri uri; Int no; "end"|] -> 
-        Manga.fromUri uri >>= Manga.downloadFromTo (no-1) Int32.MaxValue |> ignore
-    | [|Uri uri; Int start; Int stop|] -> 
-        Manga.fromUri uri >>= Manga.downloadFromTo (start-1) (stop-1) |> ignore
+    | [|Uri uri|]                      -> showChapters uri
+    | [|Uri uri; "all"|]               -> fromTo uri 0 Int32.MaxValue
+    | [|Uri uri; Int no|]              -> fromTo uri (no-1) (no-1)
+    | [|Uri uri; Int no; "end"|]       -> fromTo uri (no-1) Int32.MaxValue
+    | [|Uri uri; Int start; Int stop|] -> fromTo uri (start-1) (stop-1)
     | _ -> showUsage ()
     0 // return an integer exit code
